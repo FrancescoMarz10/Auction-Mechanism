9568fa8e76f1a0305193717500330ca6521ada1c
Francesco Marzullo <francescomarz96@gmail.com>
2020-01-11 22:41:40 +0100

    Update AuctionImplementationTest.java

--- src/test/java/AuctionImplementationTest.java
+++ src/test/java/AuctionImplementationTest.java
@@ -268,17 +268,12 @@ public class AuctionImplementationTest {
     @Test
     public void N_leaveTheNetworkAsBestBidder(){
         try {
-            Date date = null;
-            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-            date = formatter.parse("22/12/2030");
-            date.setHours(11);
-            date.setMinutes(30);
-            peer0.createAuction("Play Station 4", date, 200, "Console Sony!");
+            peer0.createAuction("Play Station 4", new Date(Calendar.getInstance().getTimeInMillis() + 8000), 200, "Console Sony!");
             peer1.placeAbid("Play Station 4", 300);
             peer2.placeAbid("Play Station 4", 350);
-            Thread.sleep(2000);
             assertTrue( peer2.exit());
-            assertEquals("The auction is active until Sun Dec 22 11:30:00 CET 2030 and the highest offer is: 200.0", peer0.checkAuction("Play Station 4"));
+            Thread.sleep(4000);
+            assertEquals("The Auction is ended with no winner!", peer0.checkAuction("Play Station 4"));
         }
         catch(Exception e) {
             e.printStackTrace();
