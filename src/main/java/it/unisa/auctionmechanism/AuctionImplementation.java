bf5da686405ae1f2037d8366294c297f1d57edd4
Francesco Marzullo <francescomarz96@gmail.com>
2020-01-11 22:33:21 +0100

    Update AuctionImplementation.java

--- src/main/java/it/unisa/auctionmechanism/AuctionImplementation.java
+++ src/main/java/it/unisa/auctionmechanism/AuctionImplementation.java
@@ -420,10 +420,6 @@ public class AuctionImplementation implements AuctionMechanism {
                             Auction auction = (Auction) futureGet.dataMap().values().iterator().next().object();
                           
                             Date actual_date = new Date();
-                           
-                              
-   
-                            
                             if(auction.get_creator()==peer_id){
                                
                                 if (!actual_date.after(auction.get_end_time())) {
