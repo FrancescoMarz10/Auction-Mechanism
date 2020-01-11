be64179df9f5de4aaee20b2627aa17d58a095f0f
Francesco Marzullo <francescomarz96@gmail.com>
2020-01-11 23:10:52 +0100

    Update AuctionImplementation.java

--- src/main/java/it/unisa/auctionmechanism/AuctionImplementation.java
+++ src/main/java/it/unisa/auctionmechanism/AuctionImplementation.java
@@ -420,11 +420,10 @@ public class AuctionImplementation implements AuctionMechanism {
                             Auction auction = (Auction) futureGet.dataMap().values().iterator().next().object();
                             Date actual_date = new Date();
                             if(auction.get_creator()==peer_id){
-
                                 if (!actual_date.after(auction.get_end_time())) {
                                     sendMessage("The auction "+ name+ " has been deleted because the creator left the network!", name,2);
-                                    removeAnAuction(name);
                                 }
+                                removeAnAuction(name);
                             }
                             if(auction.getBid_id()==peer_id){
                                 if (!actual_date.after(auction.get_end_time())) {
